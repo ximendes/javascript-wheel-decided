81e58fe0693ca5ca4861c02c5b566898247920d4
eduardo <eduardo.ximendes@4effect.com.br>
2020-07-04 15:40:09 -0300

    reponsive html

--- js/chart.js
+++ js/chart.js
@@ -1,48 +1,50 @@
 function createChart(){
 
-    var padding = {top:5, right:40, bottom:0, left:0},
-        w = 600 - padding.left - padding.right,
-        h = 600 - padding.top  - padding.bottom,
-        r = Math.min(w, h)/2,
-        rotation = 0,
-        oldrotation = 0,
-        picked = 100000,
-        oldpick = [],
-        color = d3.scale.category20();     
+    var padding = {top:5, right:40, bottom:0, left:0};
+    let width = 600 - padding.right;
+    let height = 600 ;
+    let radius = Math.min(width, height)/2;
+    let rotation = 0;
+    let oldrotation = 0;
     
+    let chartWidth = d3.select('#chart').node().getBoundingClientRect().width; 
+    let chartHeigth = chartWidth < 400 ? chartWidth : 600;
+           
     document.getElementById('chart').innerHTML = ""
         var data = app.countries;
         var svg = d3.select('#chart')
                     .append("svg")
                     .data([data])
-                    .attr("width", 600)
-                    .attr("height", 600);
-        
+                    .attr("width", chartWidth)
+                    .attr("height", chartHeigth)
+                    .attr("viewBox", `0 0 600 600`);
+
         var container = svg.append("g")
                            .attr("class", "chartholder")
-                           .attr("transform", "translate(" + (w/2) + "," + (h/2) + ")");
+                           .attr("transform", "translate(" + (width/2) + "," + (height/2) + ")");
         
         var vis = container.append("g");
             
         var pie = d3.layout.pie().sort(null).value(function(d){return 1;});
         // declare an arc generator function
-        var arc = d3.svg.arc().outerRadius(r);
+        var arc = d3.svg.arc().outerRadius(radius);
         // select paths, use arc generator to draw
         var arcs = vis.selectAll("g.slice")
                       .data(pie)
                       .enter()
                       .append("g")
-                      .attr("class", "slice");
-            
+                      .attr("class", "slice container");
+
+        let color = d3.scale.category20();            
         arcs.append("path")
             .attr("fill", function(d, i){ return color(i); })
             .attr("d", function (d) { return arc(d); });
         // add the text
         arcs.append("text").attr("transform", function(d){
                 d.innerRadius = 0;
-                d.outerRadius = r;
+                d.outerRadius = radius;
                 d.angle = (d.startAngle + d.endAngle)/2;
-                return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius -50) +")";
+                return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius -10) +")";
             }).attr("text-anchor", "end")
               .text( function(d, i) {
               return data[i].label;
@@ -51,9 +53,9 @@ function createChart(){
 
         //make arrow
         svg.append("g")
-            .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h/2)+padding.top) + ")")
+            .attr("transform", "translate(" + (width + padding.right) + "," + ((height/2)+padding.top) + ")")
             .append("path")
-            .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
+            .attr("d", "M-" + (radius*.15) + ",0L0," + (radius*.05) + "L0,-" + (radius*.05) + "Z")
             .style({"fill":"black"});
         //draw spin circle
         container.append("circle")
